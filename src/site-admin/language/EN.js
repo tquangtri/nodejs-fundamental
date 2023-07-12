39241c634be087be584916bd6f1d47e450d26ee4
terrordrone4 <70142260+terrordrone4@users.noreply.github.com>
2023-07-12 19:43:43 +0700

    first commit

---
+++ src/site-admin/language/EN.js
@@ -0,0 +1,8 @@
+const messages = {
+  register: "Register",
+  name1: "Name(first)",
+  name2: "Name(last)",
+  required: "required",
+  term1: "<li>I am ANNV</li>",
+};
+export default messages;
---
+++ src/site-customer/language/JP.js
@@ -0,0 +1,8 @@
+const messages = {
+  register: "新規登録",
+  name1: "お名前(性)",
+  name2: "お名前(名)",
+  required: "必須",
+  term1: "<li>私はANNVです</li>",
+};
+export default messages;
